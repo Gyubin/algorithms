24451a4e68b46ca5d660fc93721dbd950e4e6d65
Gyubin Son <geubin0414@gmail.com>
2020-02-12 03:47:20 +0900

    Create mode_gyubin.c

---
+++ mode/mode_gyubin.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+/*
+https://swexpertacademy.com/
+최빈수 구하기
+*/
+
+int main(void)
+{
+	int test_case;
+	int T;
+	int score, mode_score, mode_count;
+
+	setbuf(stdout, NULL);
+	scanf("%d", &T);
+
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		scanf("%d", &test_case);
+
+		int arr[101] = { 0 };
+		mode_score = -1;
+		mode_count = -1;
+		for (int i = 0; i < 1000; i++)
+		{
+			scanf("%d", &score);
+			arr[score]++;
+		}
+
+		for (int i = 0; i < 101; i++)
+		{
+			if (arr[i] >= mode_count)
+			{
+				mode_count = arr[i];
+				mode_score = i;
+			}
+		}
+		printf("#%d %d\n", test_case, mode_score);
+	}
+	return 0;
+}
