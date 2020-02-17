3f5b18d9dbaed2c10c0a7d9447d503feb67ad6e9
Gyubin Son <geubin0414@gmail.com>
2020-02-17 10:12:39 +0900

    Refactor view_gyubin.cpp

--- view/view_gyubin.cpp
+++ view/view_gyubin.cpp
@@ -1,45 +1,37 @@
-#include<iostream>
-
-#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
-
-using namespace std;
-
 /*
 https://swexpertacademy.com
 1206. [S/W 문제해결 기본] 1일차 - View
 */
 
-int getMin(int pp, int p, int n, int nn)
-{
-	return MIN(MIN(MIN(pp, p), n), nn);
-}
+#include <stdio.h>
+#include <malloc.h>
 
-int main(int argc, char** argv)
+int main()
 {
-	int testCase;
+	int test_case;
 	int T;
 
-	for (testCase = 1; testCase <= 10; ++testCase)
+	for (test_case = 1; test_case <= 10; ++test_case)
 	{
 		int arr[1000] = { 0 };
-		int length, pp, p, n, nn, result, minVal;
-		
-		cin >> length;
-		for (int i = 0; i < length; i++)
-			cin >> arr[i];
+		int length, tmp, result = 0;
 
-		result = 0;
+		scanf("%d", &length);
+		for (int i = 0; i < length; i++)
+			scanf("%d", &arr[i]);
+
 		for (int i = 2; i < length - 2; i++)
 		{
-			pp = arr[i] - arr[i - 2];
-			p = arr[i] - arr[i - 1];
-			n = arr[i] - arr[i + 1];
-			nn = arr[i] - arr[i + 2];
-			minVal = getMin(pp, p, n, nn);
-			if (minVal > 0)
-				result += minVal;
+			int min_val = 99999;
+			for (int j = -2; j <= 2; j++)
+			{
+				if (j == 0) continue;
+				tmp = arr[i] - arr[i + j];
+				if (tmp < min_val) min_val = tmp;
+			}
+			if (min_val > 0) result += min_val;
 		}
-		cout << "#" << testCase << " " << result << endl;
+		printf("#%d %d\n", test_case, result);
 	}
 	return 0;
 }
