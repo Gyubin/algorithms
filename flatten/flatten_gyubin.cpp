eb364c67d2b698977d2fe5d3e0a17a8224490ba5
Gyubin Son <geubin0414@gmail.com>
2020-02-17 10:32:50 +0900

    Update flatten_gyubin.cpp

--- flatten/flatten_gyubin.cpp
+++ flatten/flatten_gyubin.cpp
@@ -1,60 +1,60 @@
-#include<iostream>
-#define SIZE 100
-
-using namespace std;
-
 /*
 https://swexpertacademy.com
 1208. [S/W 문제해결 기본] 1일차 - Flatten
 */
 
-void findIndices(int *arr, int *ptrMax, int *ptrMin)
+#include <stdio.h>
+#include <malloc.h>
+
+#define SIZE 100
+
+void findIndices(int *arr, int *ptr_max, int *ptr_min)
 {
-	int tempMax = -1, tempMin = 999;
-	int idxMax, idxMin;
+	int temp_max = -1, temp_min = 999;
+	int idx_max, idx_min;
 
 	for (int i = 0; i < SIZE; i++)
 	{
-		if (arr[i] > tempMax)
+		if (arr[i] > temp_max)
 		{
-			tempMax = arr[i];
-			idxMax = i;
+			temp_max = arr[i];
+			idx_max = i;
 		}
-		if (arr[i] < tempMin)
+		if (arr[i] < temp_min)
 		{
-			tempMin = arr[i];
-			idxMin = i;
+			temp_min = arr[i];
+			idx_min = i;
 		}
 	}
-	*ptrMax = idxMax;
-	*ptrMin = idxMin;
+	*ptr_max = idx_max;
+	*ptr_min = idx_min;
 }
 
 void dump(int *arr)
 {
-	int idxMax, idxMin;
+	int idx_max, idx_min;
 
-	findIndices(arr, &idxMax, &idxMin);
-	arr[idxMax]--;
-	arr[idxMin]++;
+	findIndices(arr, &idx_max, &idx_min);
+	arr[idx_max]--;
+	arr[idx_min]++;
 }
 
-int main(int argc, char** argv)
+int main()
 {
-	int testCase;
-	for (testCase = 1; testCase <= 10; ++testCase)
+	int test_case;
+	for (test_case = 1; test_case <= 10; ++test_case)
 	{
 		int arr[SIZE] = { 0 };
-		int dumpNum, idxMax, idxMin;
+		int dump_num, idx_max, idx_min;
 
-		cin >> dumpNum;
+		scanf("%d", &dump_num);
 		for (int i = 0; i < SIZE; i++)
-			cin >> arr[i];
-		for (int i = 0; i < dumpNum; i++)
+			scanf("%d", &arr[i]);
+		for (int i = 0; i < dump_num; i++)
 			dump(arr);
 
-		findIndices(arr, &idxMax, &idxMin);
-		cout << "#" << testCase << " " << arr[idxMax] - arr[idxMin] << endl;
+		findIndices(arr, &idx_max, &idx_min);
+		printf("#%d %d\n", test_case, arr[idx_max] - arr[idx_min]);
 	}
 	return 0;
 }
